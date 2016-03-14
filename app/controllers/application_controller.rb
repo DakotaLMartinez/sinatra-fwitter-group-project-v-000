3e4fce7bce641eba2fa93d11ce5046faa1159284
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-14 17:15:24 +0000

    adds login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,9 +30,30 @@ class ApplicationController < Sinatra::Base
       redirect :signup
     end
   end
+  
+  get "/login" do 
+    if logged_in? 
+      redirect "/tweets" 
+    else
+      erb :"users/login"
+    end
+  end
+  
+  post "/login" do 
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
 
   get "/tweets" do
-    erb :"tweets/tweets"
+    if logged_in?
+      @user = User.find(session[:id])
+      erb :"tweets/tweets"
+    end
   end
 
   helpers do
